a0176286fe34320df83eb1e89ce02dbc61ac1b45
Denperidge <cat@denperidge.com>
2024-09-10 14:58:30 +0200

    Moved parcel-optimizer-csp to main directory

--- packages/parcel-test-project/.varsrc.js
+++
@@ -1,9 +0,0 @@
-const pkg = require('./package.json')
-
-module.exports = {
-  APP_VERSION: pkg.version,
-  APP_NAME: pkg.name,
-  API_URL: process.env.API_URL || 'https://dev.api.com',
-  IS_DEV: process.env.NODE_ENV !== 'production',
-  BUILD_TIME: Date.now(),
-}
