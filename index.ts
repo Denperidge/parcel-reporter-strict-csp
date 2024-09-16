f811123c02ec24fefc4e96bf44476a9de8926de7
Denperidge <cat@denperidge.com>
2024-09-16 14:05:09 +0200

    ITS CHANGING THINGS NOT FUNCTIONAL YETBUT HEY

--- index.ts
+++ index.ts
@@ -1,30 +1,12 @@
 import * as crypto from "crypto";
 import { Reporter } from "@parcel/plugin";
-const cheerio = require('cheerio')
-const path = require('path')
+import { FileSystem } from "@parcel/fs/lib/types";
+import { writeFileSync } from "fs";
+import * as cheerio from "cheerio";
+
 
 const sha256 = x =>
   crypto.createHash('sha256').update(x, 'utf8').digest('base64')
-
-const waitForFile = (filename: string) => {
-  return new Promise(function (resolve, reject) {
-    let attempt = 0
-    const interval = setInterval(function () {
-      if (attempt >= 3) {
-        clearInterval(interval)
-        //throw new Error(`${filename} could not be read after ${attempt} attempts`);
-      }
-      /*
-      if (fs.existsSync(filename)) {
-        clearInterval(interval)
-        resolve(fs.readFileSync(filename, { encoding: 'utf-8' }))
-      } else {
-        attempt++
-      }
-      */
-    }, 1200)
-  })
-}
 
 const buildCspContents = (config: {[key:string]:string}) => {
   let buff: string[] = []
@@ -34,10 +16,41 @@ const buildCspContents = (config: {[key:string]:string}) => {
   return buff.join(';') + ';'
 }
 
-const calculateAndPushHash = (cheerioElement, contentString, destArray) => {
-  const hash = `sha256-${sha256(contentString)}`
+const calculateAndPushHash = (content: string, destArray: string[]) => {
+  const hash = `sha256-${sha256(content)}`
   destArray.push(`'${hash}'`)
-  cheerioElement.attr('integrity', hash)
+  //$(cheerioElement).attr('integrity', hash)
+}
+
+
+
+
+const parseHtml = (fs:FileSystem, contents: string, hashes: {[key:string]: string}) => {
+  const $ = cheerio.load(contents);
+  const scriptHashes: string[] = []
+    const distDir = process.env['DIST_DIR'] || 'dist/'
+    /* The host defined here will be ignored when checking for local assets
+       e.g. HOST=https://example.com
+       <script src="https://example.com/index.js"" --> "distDir/index.js"
+    */
+    const ignoreHost = process.env['HOST'] || null
+
+    $("script").map(function () {
+      const scriptSrc = $(this).attr("src");
+      if (scriptSrc) {
+        const hash = `sha256-${hashes[scriptSrc]}`
+        scriptHashes.push(`'${hash}'`)
+        $(this).attr('integrity', hash)
+      }
+      return this
+    })
+
+    const html = $.html();
+    if (!html) {
+      throw new Error("UGH")
+    }
+    return {html, scriptHashes}
+  
 }
 
 module.exports = new Reporter({
@@ -46,11 +59,37 @@ module.exports = new Reporter({
       return
     }
 
-    for (const bundle of event.bundleGraph.getBundles()) {
-      console.log("@@")
-      console.log(bundle)
-    }
+    const filesJs = event.bundleGraph.getBundles().filter((bundle) => bundle.type == "js");
+    const htmlBundles = event.bundleGraph.getBundles().filter((bundle) => bundle.type == "html");
 
-    process.exit()
+    /*
+    for (const bundle of filesJs) { 
+      bundle.traverseAssets()
+    }*/
+    
+
+    
+    for (const bundle of htmlBundles) {
+      const bundleEntry = await bundle.getMainEntry();
+      if (!bundleEntry) {
+        throw new Error(`Could not get main entry of ${bundle.filePath}`)
+      }
+
+      const hashes = {};
+      for (const dependency of bundleEntry.getDependencies()) {
+        const dependencyAsset = event.bundleGraph.getResolvedAsset(dependency, bundle)
+        if (dependencyAsset) {
+          if (dependencyAsset.type == "js") {
+            console.log(await dependencyAsset.getCode())
+            hashes[dependency.id] = sha256(await dependencyAsset.getCode());
+          }
+        }
+      }
+  
+
+      const data = parseHtml(options.outputFS, await bundleEntry.getCode(), hashes);
+      
+      writeFileSync(bundle.filePath, data.html, {});
+    }
   },
 })
