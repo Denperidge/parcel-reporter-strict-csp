b3e7585f4fa83c744b80a452d15bca4aa7cce3e6
Denperidge <cat@denperidge.com>
2024-09-16 15:51:12 +0200

    public url compatibility, cleanup

--- index.ts
+++ index.ts
@@ -6,21 +6,13 @@ import * as cheerio from "cheerio";
 const sha256 = x =>
   crypto.createHash('sha256').update(x, 'utf8').digest('base64')
 
-const buildCspContents = (config: {[key:string]:string}) => {
-  let buff: string[] = []
-  for (const key in config) {
-    buff.push(`${key} ${config[key]}`)
-  }
-  return buff.join(';') + ';'
-}
-
 const calculateAndPushHash = (content: string, destArray: string[]) => {
   const hash = `sha256-${sha256(content)}`
   destArray.push(`'${hash}'`)
   //$(cheerioElement).attr('integrity', hash)
 }
 
-function addToCsp($: cheerio.CheerioAPI, key: string, value:string) {
+function addToHtmlMetaCsp($: cheerio.CheerioAPI, key: string, value:string) {
   const cspElem = $("meta[http-equiv='Content-Security-Policy']");
   const originalCsp = cspElem.attr("content");
   let newCsp = "";
@@ -42,35 +34,36 @@ function addToCsp($: cheerio.CheerioAPI, key: string, value:string) {
   cspElem.attr("content", newCsp)
 }
 
+const getAssetUrl = ($: cheerio.CheerioAPI, elem:cheerio.Element, attr: string, ignoreHost?:string) => {
+  const url = $(elem).attr(attr);
+  if (!url) {
+    throw new Error(`Could not get ${attr} from ${$(elem).html()}`)
+  }
+  // TODO try with non-ignored url
+  if (ignoreHost) {
+    return url.replace(ignoreHost, "")
+  } else {
+    return url;
+  }
+}
 
-const parseHtml = (fs:FileSystem, contents: string, hashes: {[key:string]: string}) => {
-  const $ = cheerio.load(contents);
-  const scriptHashes: string[] = []
-    const distDir = process.env['DIST_DIR'] || 'dist/'
-    /* The host defined here will be ignored when checking for local assets
-       e.g. HOST=https://example.com
-       <script src="https://example.com/index.js"" --> "distDir/index.js"
-    */
-    const ignoreHost = process.env['HOST'] || null
+const parseHtml = (fs:FileSystem, htmlContents: string, distDir: string, ignoreHost?:string) => {
+  const $ = cheerio.load(htmlContents);
 
+  $("script").map(function () {
+    const scriptSrc = getAssetUrl($, this, "src", ignoreHost);
+    const data = fs.readFileSync("dist" + scriptSrc).toString("utf-8")
+    const hash = `sha256-${sha256(data)}`;
+    $(this).attr('integrity', hash)
+    addToHtmlMetaCsp($, "script-src", `'${hash}'`)
+    return this
+  });
 
-    $("script").map(function () {
-      const scriptSrc = $(this).attr("src");
-      if (scriptSrc) {
-        const data = fs.readFileSync("dist" + scriptSrc).toString("utf-8")
-        console.log(data)
-        const hash = `sha256-${sha256(data)}`
-        $(this).attr('integrity', hash)
-        addToCsp($, "script-src", `'${hash}'`)
-      }
-      return this
-    })
-
-    const html = $.html();
-    if (!html) {
-      throw new Error("UGH")
-    }
-    return {html, scriptHashes}
+  const html = $.html();
+  if (!html) {
+    throw new Error("UGH")
+  }
+  return {html}
   
 }
 
@@ -79,17 +72,17 @@ module.exports = new Reporter({
     if (event.type != 'buildSuccess') {
       return
     }
+
     
+
+        /* The host defined here will be ignored when checking for local assets
+       e.g. HOST=https://example.com
+       <script src="https://example.com/index.js"" --> "distDir/index.js"
+    */
+    const distDir = options.projectRoot + "/" + (options.env["DIST_DIR"] || "dist/");
 
     const filesJs = event.bundleGraph.getBundles().filter((bundle) => bundle.type == "js");
     const htmlBundles = event.bundleGraph.getBundles().filter((bundle) => bundle.type == "html");
-
-    /*
-    for (const bundle of filesJs) { 
-      bundle.traverseAssets()
-    }*/
-    
-
     
     for (const bundle of htmlBundles) {
       const bundleEntry = await bundle.getMainEntry();
@@ -97,24 +90,9 @@ module.exports = new Reporter({
         throw new Error(`Could not get main entry of ${bundle.filePath}`)
       }
 
-      const hashes = {};
-      for (const dependency of bundleEntry.getDependencies()) {
-        const dependencyAsset = event.bundleGraph.getResolvedAsset(dependency, bundle)
-        if (dependencyAsset) {
-          if (dependencyAsset.type == "js") {
-            //console.log(await dependencyAsset.getCode())
-            hashes[dependency.id] = sha256(await dependencyAsset.getCode());
-            
-          }
-        }
-      }  
+      const ignoreHost = bundle.target.publicUrl;
 
-      //options.outputFS.writeFile("test", await bundleEntry.getCode(), {});
-
-      
-      // await bundleEntry.getCode() gives temp filenames
-
-      const data = parseHtml(options.outputFS, options.outputFS.readFileSync(bundle.filePath).toString("utf-8"), hashes);
+      const data = parseHtml(options.outputFS, options.outputFS.readFileSync(bundle.filePath).toString("utf-8"), distDir, ignoreHost);
       
       options.outputFS.writeFile(bundle.filePath, data.html, {});
     }
